49dd2ec00494be99f7dbdaf922782e30181929cc
S. M. Redwan <smredwan.info@gmail.com>
2026-06-15 22:37:47 +0600

    Solve Lab Exercise 1

---
+++ Lab-Exercises/solves/sol_1_smart_survey.py
@@ -0,0 +1,24 @@
+name = str(input("What is your name?"))
+while name == "":
+    name = str(input("Please enter your name: "))
+
+age = int(input("How old are you? "))
+while age < 0 and age > 120:
+    age = int(input("Please enter a valid age: "))
+
+dev_status = bool(input("Are you a developer? (True/False) "))
+while dev_status not in [True, False]:
+    dev_status = bool(input("Please enter True or False: "))
+
+roles = {
+    "Tier 1": "Admin",
+    "Tier 2": "Standard Executive Access",
+    "Tier 3": "Guest"
+}
+
+if age < 18:
+    print(f"Hello {name}, you are a {roles['Tier 3']} with limited access.")
+elif dev_status:
+    print(f"Hello {name}, you are a {roles['Tier 1']} with full access.")
+else:
+    print(f"Hello {name}, you are a {roles['Tier 2']} with standard access.")
